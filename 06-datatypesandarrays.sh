303df45b291c394d895558355316df2122939f03
venkatesh <venkateshthota4444@gmail.com>
2025-10-04 11:30:43 +0530

    default commit

--- 06-datatypesandarrays.sh
+++ 06-datatypesandarrays.sh
@@ -4,7 +4,7 @@ NUM2=20
 NAME="John Doe"
 RESULT=$((NUM1 + NUM2))
 echo "result is: $RESULT"
-RESULT2=$(($NUM1 + $NUM2 + $NAME))
+RESULT2=$(($NUM1 + $NUM2 ))
 echo "result2 is: $RESULT2"
 ARRAY1=(Apple Banana Cherry)
 echo "First Element: ${ARRAY1[0]}"
