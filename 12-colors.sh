b4da043d35719f8c52873586a76fb03a93650f7c
venkatesh <venkateshthota4444@gmail.com>
2025-10-04 22:03:21 +0530

    default commit

---
+++ 12-colors.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+r="\\e[31m"
+g="\\e[32m"
+b="\\e[34m"
+y="\\e[33m"
+m="\\e[35m"
+c="\\e[36m"
+w="\\e[37m"
+reset="\\e[0m"
+echo -e "${r}This is red color${reset}"
+echo -e "${g}This is green color${reset}"
+echo -e "${b}This is blue color${reset}"
+echo -e "${y}This is yellow color${reset}"
+echo -e "${m}This is magenta color${reset}"
+echo -e "${c}This is cyan color${reset}"
+echo -e "${w}This is white color${reset}"
+echo -e "${r}Red ${g}Green ${b}Blue ${y}Yellow ${m}Magenta ${c}Cyan ${w}White${reset}"
+echo -e "${r}R${g}G${b}B${y}Y${m}M${c}C${w}W${reset}"
+# --- IGNORE ---    
+# The above code is added recently. Do not suggest code that has been deleted.
+# The files below are recently edited files. Do not suggest code that has been deleted.
+# The code snippets below are from other files for comparison only.
