1889aa1da3d6fbe93939f6b368c3576dfc65bb0a
venkatesh <venkateshthota4444@gmail.com>
2025-10-04 10:33:11 +0530

    default commit

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,3 +4,10 @@ USER2="Bob"
 echo "User 1: $USER1"
 echo "User 2: $USER2"
 echo "Combined Users: $USER1 and $USER2"
+
+User3=$1
+echo "User 3: $User3"
+echo "All Users: $USER1, $USER2, and $User3"
+User4=$2
+echo "User 4: $User4"
+echo "All Users Now: $USER1, $USER2, $User3, and $User4"
